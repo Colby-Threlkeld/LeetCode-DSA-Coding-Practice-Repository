1e1b340dcfff5b4eb5ccdb807dcf0895c87e748f
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-06-02 14:41:38 -0400

    Delete LeetCode-28.py
    Moved file to different location.

--- LeetCode-28.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        if haystack == needle:
-            return 0
-        
-        n, m = len(needle), len(haystack)
-        hay = haystack[0:n]
