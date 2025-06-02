67f803b749cb973e3c2a1473b49a197fa958b7d0
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-06-02 14:36:00 -0400

    Update LeetCode_9.py

--- Strings/LeetCode_9.py
+++ Strings/LeetCode_9.py
@@ -1,1 +1,11 @@
+# link: https://leetcode.com/problems/palindrome-number/description/
+# Problem: Palindrome Number 
 
+
+class Solution:
+    def isPalindrome(self, x):
+        initial = str(x)
+
+        reverse = initial[::-1]
+
+        return initial == reverse
