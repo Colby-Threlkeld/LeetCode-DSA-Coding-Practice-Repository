815744c362230decb785a1ee14dbe6c03865722e
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-06-28 14:49:58 -0400

    Create LC70_Climbing_Stairs.py

---
+++ Dynamic_Programming/LC70_Climbing_Stairs.py
@@ -0,0 +1,22 @@
+# Link: https://leetcode.com/problems/climbing-stairs/description/?envType=problem-list-v2&envId=plakya4j
+# Problem: (Best Time: 30:34)
+
+
+# **Fibonacci style problem**
+class Solution(object):
+    def climbStairs(self, n):
+        #base cases
+        if n == 1: 
+            return 1
+        if n ==2:
+            return 2
+
+        one = 1
+        two = 2
+
+        for i in range(3, n+1):
+            current = one + two
+            one = two #moves window forward
+            two = current #two holds latest result
+
+        return two
