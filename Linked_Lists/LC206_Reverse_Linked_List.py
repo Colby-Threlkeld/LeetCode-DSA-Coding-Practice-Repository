c6146f79a20df86d8772861039a20731af4cee4a
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-06-29 23:55:19 -0400

    Update LC206_Reverse_Linked_List.py

--- Linked_Lists/LC206_Reverse_Linked_List.py
+++ Linked_Lists/LC206_Reverse_Linked_List.py
@@ -1,5 +1,5 @@
 # Link: https://leetcode.com/problems/reverse-linked-list/description/?envType=problem-list-v2&envId=plakya4j
-# Problem: LeetCode 206: Reverse Linked List (Best Time: 18:46)
+# Problem: LeetCode 206: Reverse Linked List (Best Time: 17:46)
 
 class ListNode(object):
     def __init__(self, val=0, next=None):
