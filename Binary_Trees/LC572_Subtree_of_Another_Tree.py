ca1fbd1017dc8a2982b0007daad72cd8f57670d0
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-07-13 23:33:27 -0400

    Create LC572_Subtree_of_Another_Tree.py

---
+++ Binary_Trees/LC572_Subtree_of_Another_Tree.py
@@ -0,0 +1,33 @@
+'''
+Link: https://leetcode.com/problems/subtree-of-another-tree/description/?envType=problem-list-v2&envId=plakya4j
+
+Problem: LeetCode 572: Subtree of Another Tree
+
+Time Complexity: O(n*m)  Space complexity: O(n)
+'''
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        if not root:
+            return False
+
+        if self.isSameTree(root, subRoot):
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
