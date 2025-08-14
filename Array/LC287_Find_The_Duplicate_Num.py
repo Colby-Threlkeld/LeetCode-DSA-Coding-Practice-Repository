d81eda36755f3df0dc1bdac143022d89132ac5e2
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-08-14 14:18:52 -0400

    Update LC287_Find_The_Duplicate_Num.py

--- Array/LC287_Find_The_Duplicate_Num.py
+++ Array/LC287_Find_The_Duplicate_Num.py
@@ -3,7 +3,7 @@ class Solution:
         slow = nums[0]
         fast = nums[0]
 
-        while True:
+        while True: #finding the cycle to set up second loop
             slow = nums[slow]
             fast = nums[nums[fast]]
 
@@ -11,10 +11,10 @@ class Solution:
                 break
 
         slow2 = nums[0]
-        while slow != slow2:
+        while slow != slow2:#send pointer from start because:Distance from start → cycle start
             slow = nums[slow]
             slow2 = nums[slow2]
 
-        return slow
+        return slow #return slow (or slow2) when they are both at the cycle start
 
             
