ae389f7b1a5fb38fbbd1ff8cd83394524b2c02fe
Colby Threlkeld <167151394+Colby-Threlkeld@users.noreply.github.com>
2025-08-15 16:23:35 -0400

    Create LC3_Longest_Substring_Without_Repeating_Characters.py

---
+++ Strings/LC3_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set() #empty set for storing seen values
+        l = 0 #declaring left pointer
+        res = 0 #declaring base result value
+
+        #for loop to create right pointer and iterate string
+        for r in range(len(s)):
+            while s[r] in charSet: #while there is a duplicate char in set
+                charSet.remove(s[l]) #remove leftmost character
+                l += 1 #increment l forward by 1
+
+            charSet.add(s[r]) #adds r values to set
+            res = max(res, r - l + 1) #takes the base result, or current window size
+
+        return res
+
+
+'''
+time: O(n)
+'''
